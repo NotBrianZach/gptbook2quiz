753644aa07d496a6dcdc5b4463490e6ea25b6928
NotBrianZach <8660637+NotBrianZach@users.noreply.github.com>
2023-03-05 14:17:44 -0600

    update

--- readList.sh
+++ readList.sh
@@ -1,8 +1,41 @@
+#!/usr/bin/env node
+import { program } from "commander";
+import fs from "fs";
+import prompt from "prompt";
+import { Configuration, OpenAIApi } from "openai";
+import pdf_extract from "pdf-extract";
+console.log(process.argv);
 
-# # 0-
-# ebook-viewer ~/media/books/TheDividedSelf2010.epub --open-at 59
+function isValidObject(str) {
+  try {
+    const obj = JSON.parse(str);
+    return typeof obj === "object" && obj !== null;
+  } catch (e) {
+    return false;
+  }
+}
 
-# # 0-
-# xpdf ~/media/books/tcp_ip_networkadministration_3rdedition.pdf 50 -z 200
+program
+  .version("0.1.0")
+  .option("-b, --book <book>", "Book name")
+  .option("-w, --web <web>", "Shorthand for website")
+# .option("-p, --page <page>", "current page number")
+# .option("-c, --chunkSize <chunkSize>", "number of pages to read at once")
+//TODO .option("-t, --type <type>", "pdf, html")
+  .parse(process.argv);
+const options = program.opts();
+console.log(options);
+# if (!options.file) {
+#   console.error("No file specified e.g. ./.pdf");
+#   process.exit(1);
+# }
 
-# xpdf ~/media/books/LinuxProgrammingInterface2010.pdf
+if (process.env.OPENAI_API_KEY === undefined) {
+   console.log()
+   process.exit(1);
+}
+
+if (options.length() === 0) {
+     console.log("no parameters, running default ./book2quiz.sh -f ./Frankenstein.pdf")
+}
+console.log();
